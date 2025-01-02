48aa33f70db97ccc695de760f6ba121939d58c36
shaina kango <shainakango@gmail.com>
2025-01-02 15:04:33 +0530

    added sipabase auth

---
+++ lib/supabase.ts
@@ -0,0 +1,15 @@
+import {createClient} from '@supabase/supabase-js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Config from 'react-native-config';
+
+const supabaseUrl = Config.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = Config.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
